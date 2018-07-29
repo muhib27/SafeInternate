2e7d9823b5b7e077d9cf8c2d4c1c284eee485cdc
Muhib <muhib.raven@gmail.com>
2018-07-29 17:15:58 +0600

    Initial commit

---
+++ app/src/main/java/com/champs21/safeinternate/activity/SplashActivity.java
@@ -0,0 +1,73 @@
+package com.champs21.safeinternate.activity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.champs21.safeinternate.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        int SPLASH_TIME_OUT = 1500;
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+//                boolean isFirstTime = MySheardPreference.getUsingFirstTime();
+                Intent intent;
+//                if (isFirstTime) {
+//                    intent = new Intent(SplashActivity.this, LoginActivity.class);
+//                } else {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+//                }
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+    }
+
+    private void getHashKey() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("MY_KEY_HASH:",
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
+}
